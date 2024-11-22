e02067895a7d6c8f761a386021f87fba1ae497bb
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-22 13:04:59 +0700

    Add code to input

--- app/src/main/java/com/example/alab1/MainActivity.kt
+++ app/src/main/java/com/example/alab1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.alab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    val editText = findViewById<EditText>(R.id.editText)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText.setOnEditorActionListener{_, _, _ ->
+            val input = editText.text.toString().toInt()
+
+            var output: String
+            when(input)
+            {
+                1 -> output = "Понедельник"
+                2 -> output = "Вторник"
+                3 -> output = "Среда"
+                4 -> output = "Четверг"
+                5 -> output = "Пятница"
+                6 -> output = "Суббота"
+                7 -> output = "Воскресенье"
+                else -> output = "Корректный диапазон от 1 до 7"
+            }
+            true
+        }
     }
 }
